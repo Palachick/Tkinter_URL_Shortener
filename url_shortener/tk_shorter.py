1326d9c7c7aba25a0952bc1e526762b595dea1c2
paul <pav.tchistiakov@yandex.ru>
2022-05-14 14:17:20 +0300

    add basic layout

---
+++ url_shortener/tk_shorter.py
@@ -0,0 +1,33 @@
+import tkinter as tk
+import pyshorteners as sh
+
+root = tk.Tk()
+
+#WINDOW SETTINGS
+root.title("Сократить ссылку")
+root.resizable(width=False, height=False)
+root.geometry("600x300")
+
+#FUNCTIONS
+def shortener():
+    result_input = url.get()
+    shortener = sh.Shortener()
+    short_url.replace(1.0, tk.END,shortener.tinyurl.short(result_input))
+    btn['bg'] = "#aa0000"
+
+#INTERFACE
+header_before_input = tk.Label(text="Введите ссылку для сокращения",font="Consolas 20")
+url = tk.Entry(width=25,font="Consolas 20")
+btn = tk.Button(text="Сократить", command=shortener,font="Consolas 20", bg="#00aa00", fg="#fff")
+header_after_input = tk.Label(text="Готовая ссылка",font="Consolas 20")
+short_url = tk.Text(width=25, height=2,font="Consolas 20")
+
+#PACK
+header_before_input.pack(pady=10)
+url.pack()
+btn.pack(pady=5)
+header_after_input.pack(pady=10)
+short_url.pack()
+
+#RUN
+root.mainloop()
